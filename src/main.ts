1f2cb27395e894ecdde7afa76c4b5700b4d4079c
oscarheimdahl <o.heimdahl@gmail.com>
2024-02-01 21:42:25 +0100

    Add streak base

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-// @prettier-ignore
-
 import './app.css';
 import App from './App.svelte';
 
-const appDiv = document.getElementById('app');
-const app = appDiv && new App({ target: appDiv });
+const app = new App({
+  target: document.getElementById('app'),
+});
 
 export default app;
